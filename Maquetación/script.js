641216f9b212391dc0f5d7eb526e69ea9dd7c933
Nedd Chairi Muñoz <91211902+illoneddy@users.noreply.github.com>
2022-05-13 14:21:26 +0200

    Add files via upload

--- Maquetación/script.js
+++ Maquetación/script.js
@@ -3,22 +3,22 @@
 
     // r1 = document.getElementsByTagName('question')[0].getElementsByTagName('choices')[0].childNodes[1].attributes[0]
 
+    let i = 0
 
 function cargaPregunta() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
+      myFunction(this, i);
     }
   };
   xhttp.open("GET", "Preguntas1.xml", true);
   xhttp.send();
 }
-function myFunction(xml) {
-  var i
+function myFunction(xml, i) {
   var xmlDoc = xml.responseXML;
   var x = xmlDoc.getElementsByTagName("question");
-  for (i = 0; i <x.length; i++) { 
+  // for (i = 0; i <x.length; i++) { 
     q = x[i].getElementsByTagName("wording")[0].childNodes[0].nodeValue
 
     r1 = x[i].getElementsByTagName('choices')[0].childNodes[1].childNodes[0].nodeValue
@@ -26,10 +26,16 @@ function myFunction(xml) {
     r3 = x[i].getElementsByTagName('choices')[0].childNodes[5].childNodes[0].nodeValue
     r4 = x[i].getElementsByTagName('choices')[0].childNodes[7].childNodes[0].nodeValue
 
-  }
+  // }
   document.getElementById("pregunta").innerHTML = q;
   document.getElementById("btn1").innerHTML = r1;
   document.getElementById("btn2").innerHTML = r2;
   document.getElementById("btn3").innerHTML = r3;
   document.getElementById("btn4").innerHTML = r4;
+}
+
+function siguiente(){
+  i++
+  console.log(i)
+  return i
 }
